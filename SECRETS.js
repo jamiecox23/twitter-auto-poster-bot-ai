6872edab335d51ca806221f2d5d3cbf070e2d90c
jamiecox23 <creationwaves23@gmail.com>
2025-01-10 12:49:53 +0000

    Update SECRETS.js

--- SECRETS.js
+++ SECRETS.js
@@ -1,7 +1,7 @@
 //? It is advisable to use environment variables instead of directly putting secrets in repository file but I have skipped this part as it would become complicated for many.
 //? Alternatively you can download the repository instead or forking and upload it from your account and keep it private, in that way, your secrets will not be exposed to the public.
 
-const APP_KEY = "ptakelDmkN6SaRHwQq8z9OwQy;
+const APP_KEY = "ptakelDmkN6SaRHwQq8z9OwQy";
 const APP_SECRET = "yd8aOkwLYlNDKGe6UDP1N2lrAEdYIQe2qLnwKU2KrKab2ffqC8";
 const ACCESS_TOKEN = "1877623353634099200-D1DdHEs3LPFpT0hmZyFr3WDroUd7az";
 const ACCESS_SECRET = "QA0fHdfIYX7KuvXjegOwxHqE9oSPTCb3auF1nbTO53ghE";
